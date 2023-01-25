b5952f603da30c58c1576b1ff09b9f6a037fe63b
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-01-25 13:38:08 +0100

    Create a component for the contacts

--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -1,4 +1,32 @@
+import type { Contact } from "@prisma/client";
 import { api } from "../utils/api";
+import { Headline3, Message } from "./Typography";
+import Image from "next/image";
+import { Button } from "./Button";
+
+export function ContactListItem({ contact }: { contact: Contact }) {
+  return (
+    <div className="group/contact flex w-full items-center gap-4 py-3 first:pt-0">
+      <div className="relative h-10 w-10 overflow-hidden rounded-full">
+        <Image
+          src="/profile-pics/Default.png"
+          fill
+          alt=""
+          className="object-cover"
+        />
+      </div>
+      <div>
+        <Headline3>{contact.name}</Headline3>
+        <Message className="opacity-secondary">{contact.phone || "-"}</Message>
+      </div>
+      <div className="flex flex-grow items-center justify-end gap-2 opacity-0 group-hover/contact:opacity-100">
+        <Button iconSrc="/icons/Mute.svg" />
+        <Button iconSrc="/icons/Call.svg" />
+        <Button iconSrc="/icons/More.svg" />
+      </div>
+    </div>
+  );
+}
 
 export function ContactsList() {
   /**
@@ -14,11 +42,9 @@ export function ContactsList() {
 
   return (
     <div>
-      <span>
-        {contactsQuery.data?.map((contact) => (
-          <div key={contact.id}>{contact.name}</div>
-        ))}
-      </span>
+      {contactsQuery.data?.map((contact) => (
+        <ContactListItem key={contact.id} contact={contact} />
+      ))}
     </div>
   );
 }
