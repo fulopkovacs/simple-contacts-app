7a3db2c354688f358307344c36686a76f9cbc63b
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-01-26 11:23:14 +0100

    Animate the appearance of the AddContactDialog

--- src/components/AddContactDialog.tsx
+++ src/components/AddContactDialog.tsx
@@ -69,18 +69,39 @@ export function AddContactDialog() {
 
   const filePickerInput = useRef<HTMLInputElement>(null);
 
+  if (!dialogOpen) return <></>;
+
   return (
     <>
-      {dialogOpen && (
-        <>
-          <div
-            className="fixed top-0 left-0 z-20 h-screen w-screen bg-black opacity-40"
-            onClick={() => closeDialog()}
-          />
-          <div
+      <AnimatePresence>
+        <motion.div
+          className="fixed top-0 left-0 z-20 h-screen w-screen bg-black opacity-40"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.4 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          key="add-contact-dialog-overlay"
+        />
+        <div
+          className="top fixed top-0 left-0 z-20 flex h-screen w-screen items-center justify-center bg-transparent"
+          onClick={
+            // Close the dialog when user clicks outside of the dialog
+            () => closeDialog()
+          }
+        >
+          <motion.div
             aria-labelledby="add-contact-dialog-title"
             role={"dialog"}
-            className="fixed top-1/2 left-1/2 z-30 grid w-96 -translate-x-1/2 -translate-y-1/2 gap-6 bg-g-100 p-6 text-white"
+            className="fixed z-30 grid w-96 -translate-x-1/2 -translate-y-1/2 gap-6 bg-g-100 p-6 text-white"
+            initial={{
+              opacity: 0,
+              y: 10,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{ duration: 0.2, delay: 0.1 }}
             onKeyUp={(e) => {
               // Close the dialog with "Esc"
               if (e.key === "Escape") {
@@ -88,27 +109,30 @@ export function AddContactDialog() {
                 e.stopPropagation();
               }
             }}
+            key="add-contact-dialog-content"
+            onClick={
+              // Do not close the dialog if it is clicked
+              (e) => e.stopPropagation()
+            }
           >
             <Headline2 id="add-contact-dialog-title">Add contact</Headline2>
             <div className="flex items-center gap-4">
               <div className="relative h-20 w-20 overflow-hidden rounded-full">
-                <AnimatePresence>
-                  <motion.div
-                    className="absolute h-full w-full"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.3 }}
-                    key={profilePictureURL || "default-profile-pic"}
-                  >
-                    <Image
-                      src={profilePictureURL || "/profile-pics/Default.png"}
-                      alt=""
-                      fill
-                      className="object-cover"
-                    />
-                  </motion.div>
-                </AnimatePresence>
+                <motion.div
+                  className="absolute h-full w-full"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  key={profilePictureURL || "default-profile-pic"}
+                >
+                  <Image
+                    src={profilePictureURL || "/profile-pics/Default.png"}
+                    alt=""
+                    fill
+                    className="object-cover"
+                  />
+                </motion.div>
               </div>
               <input
                 onChange={handleImageFile}
@@ -171,9 +195,9 @@ export function AddContactDialog() {
                 Done
               </Button>
             </div>
-          </div>
-        </>
-      )}
+          </motion.div>
+        </div>
+      </AnimatePresence>
     </>
   );
 }
