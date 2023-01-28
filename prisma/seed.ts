b5e829b26f1af01f739030b8f6356cb6c5e311b6
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-01-28 14:13:10 +0100

    Update the db seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,9 @@
 import { prisma } from "../src/server/db";
+import { images } from "./base64images";
 
 async function main() {
   const userId = "39ad5f1f-7da1-48f4-8de3-ff29de51d5c5";
+  const phone = "+36012345678";
   await prisma.user.upsert({
     where: {
       id: userId,
@@ -13,32 +15,61 @@ async function main() {
     update: {},
   });
 
-  [
+  const initialContacts = [
     {
-      name: "John Doe",
-      phone: "+36014435657",
-      email: "john@example.com",
+      name: "Timothy Lewis",
+      phone,
+      profilePhoto: images.timothy,
+      email: "timothy@mail.com",
+      id: "5c653efa-bd1c-4e76-b411-3cda06597b1a",
+    },
+    {
+      name: "Sarah Wright",
+      phone,
+      profilePhoto: images.sarah,
+      email: "sarah@mail.com",
+      id: "5c653efa-bd1c-4e76-b411-3cda06597b1b",
+    },
+    {
+      name: "Lucy Jones",
+      phone,
+      profilePhoto: images.lucy,
+      email: "lucy@mail.com",
       id: "5c653efa-bd1c-4e76-b411-3cda06597b1c",
     },
     {
-      name: "Alexandra Smith",
-      phone: "+36304444444",
-      email: "sandra@example.com",
-      id: "84502234-982f-4e83-bee2-b50327e1d85a",
+      name: "Jake Perez",
+      phone,
+      profilePhoto: images.jake,
+      email: "jake@mail.com",
+      id: "5c653efa-bd1c-4e76-b411-3cda06597b1d",
     },
-  ].map(async ({ name, phone, email, id }) => {
+    {
+      name: "Adebayo Rodriguez",
+      phone,
+      profilePhoto: images.adebayo,
+      email: "adebayo@mail.com",
+      id: "5c653efa-bd1c-4e76-b411-3cda06597b1e",
+    },
+  ]
+
+  // Prisma has a bug with async operations in an SQLite db,
+  // so we have to use a for loop here (instead of Promise.all):
+  // https://github.com/prisma/prisma/issues/9562#issuecomment-1051189816
+  for (const { name, phone, email, id, profilePhoto } of initialContacts ) {
     await prisma.contact.upsert({
       where: { id },
       create: {
         name,
         phone,
         email,
+        profilePhoto,
         id,
         userId,
       },
       update: {},
     });
-  });
+  }
 }
 
 main()
