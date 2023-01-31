e2c88b2583864971f4fb9a0333040c0fcdb9f6df
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-01-31 16:37:08 +0100

    Fix the background gradient of the "Add new"-button to match the design spec.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -48,13 +48,18 @@ export function EditContactButton({
 }: Omit<ButtonProps, "primary" | "iconSrc">) {
   return (
     <button
-      className={`type-body relative rounded-full bg-gradient-to-b from-add-contact-border-1 to-add-contact-border-2 ${
+      className={`type-body add-contact-gradient-border relative rounded-full ${
         className || ""
       }`}
       {...props}
     >
+      {/*
+      NOTE: The `add-contact-gradient-base` and `add-contact-gradient-border` custom Tailwind classes
+      take care of the special gradient background of the "Add new"-button.
+      Check out the `src/styels/globals.css` for these classes.
+      */}
       <span className="absolute top-0 left-0 z-10 h-full w-full rounded-full bg-white opacity-0 transition-opacity hover:opacity-[4%]"></span>
-      <div className="relative m-[1px] flex h-10 w-max items-center justify-center gap-2 rounded-full bg-gradient-to-b from-add-contact-base-1 to-add-contact-base-2  pl-3 pr-4">
+      <div className="add-contact-gradient-base relative m-[1px] flex h-10 w-max items-center justify-center gap-2 rounded-full  pl-3 pr-4">
         <span className="relative inline-block h-6 w-6">
           <Image src="/icons/Add.svg" alt="icon" fill priority />
         </span>
