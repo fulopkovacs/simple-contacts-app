410bc02c51dc71fffc4280125e43281b325fd37e
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-01-31 19:19:01 +0100

    Use valid phone numbers for the seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,7 @@ import { images } from "./base64images";
 
 async function main() {
   const userId = "39ad5f1f-7da1-48f4-8de3-ff29de51d5c5";
-  const phone = "+36012345678";
+  const phone = "+3612345678";
   await prisma.user.upsert({
     where: {
       id: userId,
@@ -51,12 +51,12 @@ async function main() {
       email: "adebayo@mail.com",
       id: "5c653efa-bd1c-4e76-b411-3cda06597b1e",
     },
-  ]
+  ];
 
   // Prisma has a bug with async operations in an SQLite db,
   // so we have to use a for loop here (instead of Promise.all):
   // https://github.com/prisma/prisma/issues/9562#issuecomment-1051189816
-  for (const { name, phone, email, id, profilePhoto } of initialContacts ) {
+  for (const { name, phone, email, id, profilePhoto } of initialContacts) {
     await prisma.contact.upsert({
       where: { id },
       create: {
